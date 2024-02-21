8c28b95c48d2e928c951e36f01828b0d56767d7c
dev kartik <1kartikshettar@gmail.com>
2024-02-21 14:28:04 +0530

    project setup

---
+++ src/components/Cards/MiniCard.jsx
@@ -0,0 +1,7 @@
+function MiniCard() {
+  return (
+    <div>MiniCard</div>
+  )
+}
+
+export default MiniCard
---
+++ src/components/Cards/ReuseableCard.jsx
@@ -0,0 +1,7 @@
+function ReuseableCard() {
+  return (
+    <div>ReuseableCard</div>
+  )
+}
+
+export default ReuseableCard
